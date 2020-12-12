1d85a3e54aeccfd51c4334524f84a23c4c7c6204
ritikvashistha <ritik@sutratechlabs.com>
2020-12-12 22:46:09 +0530

    Updates

---
+++ precache-manifest.c20f1954e5074a04dca355b1fa6c3447.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c028a6bf96199071785b4508054ca18f",
+    "url": "/thunkpost/index.html"
+  },
+  {
+    "revision": "f7c59b1f6c79e19be42f",
+    "url": "/thunkpost/static/css/main.f5b39a3c.chunk.css"
+  },
+  {
+    "revision": "5dd70757d364d6fb292d",
+    "url": "/thunkpost/static/js/2.5a7be56c.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/thunkpost/static/js/2.5a7be56c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7c59b1f6c79e19be42f",
+    "url": "/thunkpost/static/js/main.7ea4ab15.chunk.js"
+  },
+  {
+    "revision": "d48c1cfd0f42306d1694",
+    "url": "/thunkpost/static/js/runtime-main.4f702c6c.js"
+  }
+]);
